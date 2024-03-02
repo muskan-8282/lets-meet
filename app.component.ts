9adf8da44f7bc166807ac8ac8fded28a997d819e
Muskan Sharma <muskansharma8282@gmail.com>
2024-03-02 18:18:44 +0530

    Standalone component setup.

--- app.component.ts
+++ app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { LandingPageComponent } from "./landing-page/landing-page.component";
+import { ProfileComponent } from "./profile/profile.component";
+import { SigninComponent } from "./signin/signin.component";
+import { ContactUsComponent } from "./contact-us/contact-us.component";
+import { LoginComponent } from "./login/login.component";
+import { JoinMeetupComponent } from "./join-meetup/join-meetup.component";
+import { CreateMeetupComponent } from "./create-meetup/create-meetup.component";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
+  imports: [
+    LandingPageComponent,
+    ProfileComponent,
+    SigninComponent,
+    ContactUsComponent,
+    LoginComponent,
+    JoinMeetupComponent,
+    CreateMeetupComponent,
+  ],
 })
 export class AppComponent {
-  title = 'EncounterBox';
+  title = "EncounterBox";
 }
---
+++ app.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { LandingPageComponent } from "./landing-page/landing-page.component";
+import { BrowserModule } from "@angular/platform-browser";
+
+@NgModule({
+  imports: [BrowserModule],
+  declarations: [],
+  bootstrap: [],
+})
+export class AppModule {}
